87960c827d2fc9ae7f2170690f2c521e2d03e1ae
Saeeds28 <awesomesaad.saeed2@gmail.com>
2019-11-02 19:39:15 -0400

    using pathparam to fetch a single instance of an alien object

--- RestDemo/src/main/java/com/saeeds28/RestDemo/AliensResource.java
+++ RestDemo/src/main/java/com/saeeds28/RestDemo/AliensResource.java
@@ -8,6 +8,7 @@ import java.util.List;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 
 @Path("aliens") // will call this class whenever this url is specified
@@ -16,13 +17,20 @@ public class AliensResource {
 	
 	@GET	// this method will get called whenever a get request is generated
 	@Produces(MediaType.APPLICATION_XML) // server returns a resource in an xml format
-	public List<Alien> getAlien() {
-		System.out.println("getAlien method invoked");
+	public List<Alien> getAliens() {
+		System.out.println("getAliens method invoked");
 		return repo.getAliens();
 	}
 	
+	@GET	// this method will get called whenever a get request is generated
+	@Path("alien/{id}")	// id is now a placeholder
+	@Produces(MediaType.APPLICATION_XML) // server returns a resource in an xml format
+	// PathParam maps the placeholder to the method argument
+	public Alien getAlien(@PathParam("id") int id) {
+		return repo.getAlien(id);
+	}
 	
-	@POST	// creating our own resource in the server
+	@POST	// creating our own resource on the server
 	@Path("alien")	// will be called when aliens/alien is visited
 	// Client: use postman to type in the information in xml/json format
 	public Alien createAlien(Alien a) {
